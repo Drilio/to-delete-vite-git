8117b47ac53ddf530eedfaa9d0401e7117cfdcd4
Antoine <antoineroy92@gmail.com>
2023-09-22 12:23:16 +0200

    ajout de la fonctionalité onclick inprogress

--- main.js
+++ main.js
@@ -11,7 +11,12 @@ text.setAttribute('type','text');
 
 const submit = document.createElement('button');
 submit.setAttribute('type', 'submit');
+submit.setAttribute('id', 'submit-button');
 submit.innerHTML = "Submit";
+
+const toDoList = document.createElement('div');
+toDoList.setAttribute('id', 'to-do');
+
 
 app.appendChild(div);
 
@@ -20,5 +25,19 @@ div.appendChild(form);
 form.appendChild(label);
 form.appendChild(text);
 form.appendChild(submit);
+
+div.appendChild(toDoList);
+
+document.getElementById('submit-button').addEventListener("click", submitTodo);
+
+function submitTodo(){
+    const toDo = document.createElement('p');
+    toDo.innerHTML = text.innerText;
+    toDoList.appendChild(toDo);
+}
+
+
+
+
 
 
