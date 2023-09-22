22608537d9abe654b491402d40cbb018957ccf0b
Antoine <antoineroy92@gmail.com>
2023-09-22 11:56:32 +0200

    feat textbox

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-console.log('toto');
-console.log("philippe");
-console.log('patate');
+const app = document.querySelector('#app');
+
+const div = document.createElement('div');
+div.classList.add('container');
+
+const form = document.createElement('form');
+
+const text = document.createElement('imput');
+text.setAttribute('type','text');
+
+app.appendChild(div);
+
+div.appendChild(form);
+
+form.appendChild(text);
