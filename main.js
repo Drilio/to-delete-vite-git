285f97b8c9233c64840af7c1ca4037ad8f6889eb
Antoine <antoineroy92@gmail.com>
2023-09-22 12:03:53 +0200

    feat textbox

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ const div = document.createElement('div');
 div.classList.add('container');
 
 const form = document.createElement('form');
+const label = document.createElement('label');
 
-const text = document.createElement('imput');
+const text = document.createElement('input');
 text.setAttribute('type','text');
 
 app.appendChild(div);
 
 div.appendChild(form);
 
+form.appendChild(label);
 form.appendChild(text);
